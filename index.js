0cf0ed668f986c4a0e1084c5cc408502f17b8c38
daniel.freitas <daniel.freitas@movile.com>
2021-07-11 17:18:45 -0300

    added Usuario type and resolve an field

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const {ApolloServer, gql} = require ('apollo-server')
 
 const typeDefs = gql`
+
+    type Usuario {
+        id: ID
+        nome: String
+        email: String
+        idade: Int
+        salario: Float
+        vip: Boolean
+    }
+
     type Query {
         ola: String
         horaCerta: String
+        funcionario: Usuario
     }
 `
 
 const resolvers = {
+    Usuario: {
+        salario (obj) {
+            return obj.salario_atual
+        }
+    },
+
     Query: {
         ola() {
             return 'Parabéns, sua primeira consulta em graphQl :)'
@@ -15,9 +32,18 @@ const resolvers = {
 
         horaCerta() {
             return new Date().toLocaleDateString()
+        },
+
+        funcionario () {
+            return {
+                id: 1,
+                nome: 'Jorgin da Sul',
+                idade: 27,
+                salario_atual: 10561.20,
+                vip: true
+            }
         }
     }
-    
 }
 
 const server = new ApolloServer ({
